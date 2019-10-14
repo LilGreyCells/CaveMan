8bc80156e3ec33a21f683f67df82ff73e7dfda89
Dishant Kaushik <dkaushik95@gmail.com>
2019-10-13 22:48:39 -0400

    Added DeerRun Script

---
+++ Assets/DeerRun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerRun : MonoBehaviour
+{
+  public GameObject upperLegBackBackground;
+  public GameObject upperLegBackForeground;
+  public GameObject upperLegFrontBackground;
+  public GameObject upperLegFrontForeground;
+  public GameObject lowerLegBackBackground;
+  public GameObject lowerLegBackForeground;
+  public GameObject lowerLegFrontBackground;
+  public GameObject lowerLegFrontForeground;
+  public GameObject deer;
+
+  public float rotationMax = 150f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
