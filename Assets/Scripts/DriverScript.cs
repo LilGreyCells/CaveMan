e9eb7fd662efbc9ef2e14ac6a0c3d0674ee2a8b3
Gourav <kvoththebloodless@gmail.com>
2019-11-21 19:13:21 -0500

    Deer Walk implemented

--- Assets/Scripts/DriverScript.cs
+++ Assets/Scripts/DriverScript.cs
@@ -8,13 +8,13 @@ public class DriverScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        deer.addAnimation(false, 5f, 0, "DeerRun.anim", 0.2f, false);
-        deer.addAnimation(false, 1f, 0, "DeerRuntoStandingRun.anim", 1f, true);
-        deer.addAnimation(true, 5f, 0, "DeerStandingRunning.anim", 0.2f,false);
+        /* deer.addAnimation(false, 5f, 0, "DeerRun.anim", 0.2f, false);
+         deer.addAnimation(false, 1f, 0, "DeerRuntoStandingRun.anim", 1f, true);
+         deer.addAnimation(true, 5f, 0, "DeerStandingRunning.anim", 0.2f,false);*/
+        deer.addAnimation(true, 10f, 0, "DeerWalking.anim", 2f, false);
 
 
 
-       
         deer.playStart();
 
     }
