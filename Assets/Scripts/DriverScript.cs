fdb44be59ac75abc395a853755b5f1dd851dec3b
Gourav <kvoththebloodless@gmail.com>
2019-11-21 21:14:17 -0500

    Deer Laughing updated

--- Assets/Scripts/DriverScript.cs
+++ Assets/Scripts/DriverScript.cs
@@ -12,9 +12,9 @@ public class DriverScript : MonoBehaviour
         deer.addAnimation(false, 1f, 0, "DeerRuntoStandingRun.anim", 1f, true);
         deer.addAnimation(true, 5f, 0, "DeerStandingRunning.anim", 0.2f, false);*/
 
-        deer.addAnimation(false, 5f, 0, "DeerGrazings.anim", 2f, false);
-        deer.addAnimation(false, 5f, 0, "DeerWalking.anim", 2f, false);
-        deer.addAnimation(false, 5f, 0, "DeerGrazings.anim", 2f, false);
+       /* deer.addAnimation(false, 5f, 0, "DeerGrazings.anim", 2f, false);
+        deer.addAnimation(false, 5f, 0, "DeerWalking.anim", 2f, false);*/
+        deer.addAnimation(false, 10f, 0, "DeerLaughing.anim", 1f, false);
 
 
 
